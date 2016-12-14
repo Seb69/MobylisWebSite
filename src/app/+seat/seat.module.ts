a2e03269262a38b21f566dc5cfcec86c2783c53c
Sebastien <sebastien.andre.seb@gmail.com>
2016-12-14 18:45:20 +0100

    add seat

---
+++ src/app/+seat/seat.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SeatComponent } from './seat.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [SeatComponent]
+})
+export class SeatModule { }
